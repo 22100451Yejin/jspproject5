61c5e1824475d7f2ce0dcd0e7a0f5d7b6054dea9
22100451Yejin <22100451@handong.ac.kr>
2022-12-10 17:19:57 +0900

    make MybatisProject

--- src/main/java/com/example/BoardDAO.java
+++ src/main/java/com/example/BoardDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.BoardVO;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,56 +17,73 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BoardDAO {
-	Connection conn = null;
-	PreparedStatement stmt = null;
-	ResultSet rs = null;
+//	Connection conn = null;
+//	PreparedStatement stmt = null;
+//	ResultSet rs = null;
 
 	@Autowired
-	private JdbcTemplate template;
+	SqlSession sqlSession;
 
-	public void setTemplate(JdbcTemplate template) {
-		this.template = template;
-	}
-
-	private final String BOARD_INSERT = "insert into BOARD (category,title, writer, content) values (?,?,?,?)";
-	private final String BOARD_UPDATE = "update BOARD set category=?, title=?, writer=?, content=? where seq=?";
-	private final String BOARD_DELETE = "delete from BOARD  where seq=?";
-	private final String BOARD_GET = "select * from BOARD  where seq=?";
-	private final String BOARD_LIST = "select * from BOARD order by seq desc";
+//	private final String BOARD_INSERT = "insert into BOARD (category,title, writer, content) values (?,?,?,?)";
+//	private final String BOARD_UPDATE = "update BOARD set category=?, title=?, writer=?, content=? where seq=?";
+//	private final String BOARD_DELETE = "delete from BOARD  where seq=?";
+//	private final String BOARD_GET = "select * from BOARD  where seq=?";
+//	private final String BOARD_LIST = "select * from BOARD order by seq desc";
 
 	public int insertBoard(BoardVO vo) {
-		return template.update(BOARD_INSERT, new Object[]{vo.getCategory(),vo.getTitle(),vo.getWriter(),vo.getContent()});
+		int count=sqlSession.insert("Board.insertBoard",vo);
+		return count;
 	}
 
-	public int deleteBoard(int id) {
-		return template.update(BOARD_DELETE, new Object[]{id});
+	public BoardVO getBoard(int seq){
+		BoardVO one=sqlSession.selectOne("Board.getBoard",seq);
+		return one;
+	}
+
+	public List<BoardVO> getBoardList(){
+		List<BoardVO> list=sqlSession.selectList("Board.getBoardList");
+		return list;
 	}
 
 	public int updateBoard(BoardVO vo) {
-		return template.update(BOARD_UPDATE, new Object[]{vo.getCategory(),vo.getTitle(),vo.getWriter(),vo.getContent(),vo.getSeq()});
+		int count=sqlSession.update("Board.updateBoard",vo);
+		return count;
 	}
 
-	public BoardVO getBoard(int seq) {
-		return template.queryForObject(BOARD_GET,
-				new Object[] {seq},
-				new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
-		}
-	
-	public List<BoardVO> getBoardList(){
-	return template.query(BOARD_LIST, new RowMapper<BoardVO>() {
-		@Override
-		public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-			BoardVO data=new BoardVO();
-			data.setSeq(rs.getInt("seq"));
-			data.setCategory(rs.getString("category"));
-			data.setContent(rs.getString("content"));
-			data.setTitle((rs.getString("title")));
-			data.setRegdate(rs.getDate("regdate"));
-			data.setWriter(rs.getString("writer"));
-			return data;
-		}
-	});
+	public int deleteBoard(int seq){
+		int result=sqlSession.delete("Board.deleteBoard",seq);
+		return result;
 	}
+
+//	public int deleteBoard(int id) {
+//		return template.update(BOARD_DELETE, new Object[]{id});
+//	}
+//
+//	public int updateBoard(BoardVO vo) {
+//		return template.update(BOARD_UPDATE, new Object[]{vo.getCategory(),vo.getTitle(),vo.getWriter(),vo.getContent(),vo.getSeq()});
+//	}
+//
+//	public BoardVO getBoard(int seq) {
+//		return template.queryForObject(BOARD_GET,
+//				new Object[] {seq},
+//				new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
+//		}
+//
+//	public List<BoardVO> getBoardList(){
+//	return template.query(BOARD_LIST, new RowMapper<BoardVO>() {
+//		@Override
+//		public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//			BoardVO data=new BoardVO();
+//			data.setSeq(rs.getInt("seq"));
+//			data.setCategory(rs.getString("category"));
+//			data.setContent(rs.getString("content"));
+//			data.setTitle((rs.getString("title")));
+//			data.setRegdate(rs.getDate("regdate"));
+//			data.setWriter(rs.getString("writer"));
+//			return data;
+//		}
+//	});
+//	}
 }
 
 
